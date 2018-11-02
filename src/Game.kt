f5509978eaa5f2af67cfa917dcaf9b31dcfd2e83
Kenneth Barker <krbarker@gmail.com>
2018-11-01 20:59:43 -0400

    Added player health conditionals

--- src/Game.kt
+++ src/Game.kt
@@ -1,10 +1,31 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var healthPoints = 100
+    var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
 
-    if (healthPoints == 100) {
-        println(name + " is in excellent condition!")
+    // Aura
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    println(auraColor)
+
+    // Health status
+    val healthStatus = if (healthPoints == 100) {
+        "is in excellent condition!"
+    } else if (healthPoints >= 90) {
+        "has a few scratches."
+    } else if (healthPoints >= 75) {
+        if (isBlessed) {
+            "has some minor wounds but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+    } else if (healthPoints >= 15) {
+        "looks pretty hurt."
     } else {
-        println(name + " is in awful condition!")
+        "is in awful condition!"
     }
+
+    // Player status
+    println(name + " " + healthStatus)
 }
